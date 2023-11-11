ab3031da9d3abf1eeddfa8d284daf9474c40b76c
Aleksandr <alex.avramenko2004@gmail.com>
2023-11-11 17:31:14 -0500

    Success block added

--- src/components/success/Success.js
+++ src/components/success/Success.js
@@ -2,9 +2,26 @@ import './Success.css'
 
 const Success = () => {
     return (
-        <>
-            
-        </>
+        <section className="success">
+            <div className="success-div">
+                <h3 className="success-heading">Успіхи наших учнів</h3>
+
+                <div className="success-icons">
+                    <div className="success-icon">
+                        <p className="success-text">99%</p>
+                        <p>успішно здали ДПА/ЗНО після проходження наших курсів</p>
+                    </div>
+                    <div className="success-icon">
+                        <p className="success-text">96%</p>
+                        <p>залишили позитивний відгук про наші курси</p>
+                    </div>
+                    <div className="success-icon">
+                        <p className="success-text">98%</p>
+                        <p>стали більш впевненими в своїх знаннях після проходження <br/> наших курсів</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
